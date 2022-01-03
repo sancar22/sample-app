8f6aff52ebe320346488f778f28d3388ebebad4a
Admin <admin@Admiins-MacBook-Air.local>
2022-01-03 16:35:49 -0600

    chore: finishing auth testing

--- server/test/auth.test.js
+++ server/test/auth.test.js
@@ -1,20 +1,22 @@
 const express = require('express');
-// const expect = require('chai').expect;
-var should = require('chai').should();
+const dotenv = require('dotenv')
 const jwt = require('jsonwebtoken');
 const supertest = require('supertest');
-const mocks = require('./mocks');
 const sinon = require('sinon');
-const {User} = require('../models/user');
 const { authMiddleware } = require('../middlewares/auth');
-// const config = require('config');
 
-// const testJWTCorrect = 'Bearer ' + jwt.sign({_id: mocks.createTestUser._id}, config.get('jwtSecret'));
-const invalidTestJWT = 'Bearer ' + mocks.invalidJWT.value;
+// Allows using Chai assertions
+require("chai").should();
+
+// env
+dotenv.config();
+
+const validTestJWT = 'Bearer ' + jwt.sign({user: 1}, process.env.JWT_SECRET);
+const invalidTestJWT = 'Bearer ' + jwt.sign({user: 1}, 'Banana');
 
 // Spy Test Function
 function testCallback(req, res) {
-  res.status(200).send('function tested');
+  res.status(200).send('test function called');
 };
 
 const testCallbackSpy = sinon.spy(testCallback);
@@ -29,10 +31,6 @@ app.get('/authTestRoute', authMiddleware, testCallbackSpy);
 const request = supertest(app);
 
 describe('test user auth', () => {
-  
-  before('create test user', async () => {
-    await User.sync();
-  })
 
   it('Should be forbidden if there is no JWT', async () => {
     const res = await request.get('/authTestRoute')
@@ -45,15 +43,11 @@ describe('test user auth', () => {
     res.status.should.equal(401);
   })
 
-//   it('should call next() if verified JWT and user', async () => {
-//     const res = await request.get('/authTestRoute')
-//       .set('Authorization', testJWTCorrect);
-//       res.status.should.equal(261);
-//       res.text.should.equal('next func reached');
-//     expect(testCallback.calledOnce).to.be.true;
-//   })
-
-//   after('remove test user', async () => {
-//     await User.findByIdAndDelete(mocks.createTestUser._id);
-//   })
+  it('should call next() if verified JWT and user', async () => {
+    const res = await request.get('/authTestRoute')
+      .set('Authorization', validTestJWT);
+      res.status.should.equal(200);
+      res.text.should.equal('test function called');
+    testCallbackSpy.calledOnce.should.be.true;
+  })
 })
