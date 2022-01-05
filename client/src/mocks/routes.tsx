436be36a3320b55e4284591123ff432153f208d3
Admin <admin@Admiins-MacBook-Air.local>
2022-01-05 13:54:52 -0600

    chore: auth msw

--- client/src/mocks/routes.tsx
+++ client/src/mocks/routes.tsx
@@ -1,5 +1,7 @@
 import { rest } from 'msw';
 
+import {FormDataRegister} from '../interfaces';
+
 const base_api_url = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export const routes = [
@@ -10,8 +12,18 @@ export const routes = [
   /*
   ---------------- START AUTH ROUTES -------------------------
   */
-  rest.post(base_api_url + '/api/auth/register', (req, res, ctx) => {}),
-  rest.post(base_api_url + '/api/auth/login', (req, res, ctx) => {}),
+ rest.post(base_api_url + '/api/auth/login', (req, res, ctx) => {
+  sessionStorage.setItem('accessToken', 'success');
+  return res(ctx.status(200), ctx.json({ res: 'success' }));
+ }),
+  rest.post<FormDataRegister>(base_api_url + '/api/auth/register', (req, res, ctx) => {
+    const { firstName, surname, username, password } = req.body;
+    if (!firstName || !surname || !username || !password) {
+      return res(ctx.status(400), ctx.json({res: 'Missing fields!', error: true}))
+    } else {
+      return res(ctx.status(201), ctx.json({res: 'User registered successfully!', error: false}))
+    }
+  }),
   /*
   ---------------- END AUTH ROUTES -------------------------
   */
