23086c8a4ac00bb4b9132e223b80876c3510ec39
Admin <admin@Admiins-MacBook-Air.local>
2022-01-05 11:31:39 -0600

    chore: initial fe test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders loging/register when the app is rendered', () => {
   render(<App />);
-  const linkElement = screen.getByText(/string/i);
-  expect(linkElement).toBeInTheDocument();
+  const registerElement = screen.getByText('Register');
+  expect(registerElement).toBeInTheDocument();
+  const loginElement = screen.getByText('Login');
+  expect(loginElement).toBeInTheDocument();
 });
